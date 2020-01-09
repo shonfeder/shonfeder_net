a94ad7ac331956e1adb3051b6516d39cd64288c0
Shon Feder <shon.feder@gmail.com>
2020-01-08 22:36:33 -0500

    Remove commented out code

--- bin/main.ml
+++ bin/main.ml
@@ -9,17 +9,11 @@ let assets =
     ~uri_prefix:"/"
     ()
 
-(* let root =
- *   get "/" (fun _req -> `Html (Content.landing_page ()) |> respond') *)
-
 let add_pages app =
   let add_page app (route, page) =
     get route (fun _req -> `Html (page ()) |> respond') app
   in
   List.fold ~f:add_page ~init:app (Content.pages ())
-
-(* let programs =
- *   get "/programs" (fun _req -> `Html (Content.programs_page ()) |> respond') *)
 
 let log_reporter () = Lwt.return (Logs_fmt.reporter ())
 
@@ -36,8 +30,6 @@ let app =
   App.empty
   |> middleware assets
   |> add_pages
-(* |> root
- * |> programs *)
 
 let run () =
   match App.run_command' app with
@@ -46,8 +38,3 @@ let run () =
   | `Not_running -> exit 0
 
 let () = run ()
-
-(* let _ = App.empty
- *         |> middleware assets
- *         |> root
- *         |> App.run_command *)
